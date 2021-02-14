8fdffc91a5f70a367835fb3de0f012661e838340
Robert Lewis <robb@ghosti.net>
2021-02-14 10:42:03 -0500

    added more fields

--- modules/custom/troth_officer/src/TrothOfficerEntityStorageSchema.php
+++ modules/custom/troth_officer/src/TrothOfficerEntityStorageSchema.php
@@ -20,6 +20,26 @@ class TrothOfficerEntityStorageSchema extends SqlContentEntityStorageSchema {
     if (!empty($schema['troth_office']['fields']['office_email'])) {
       $schema['troth_office']['fields']['office_email']['not null'] = FALSE;
     }
+    // Then target your annoying field and set the 'not null' key to FALSE!
+    if (!empty($schema['troth_office']['fields']['office_description__value'])) {
+      $schema['troth_office']['fields']['office_description__value']['not null'] = FALSE;
+    }
+    // Then target your annoying field and set the 'not null' key to FALSE!
+    if (!empty($schema['troth_office']['fields']['office_description__format'])) {
+      $schema['troth_office']['fields']['office_description__format']['not null'] = FALSE;
+    }
+    // Then target your annoying field and set the 'not null' key to FALSE!
+    if (!empty($schema['troth_office']['fields']['office_term'])) {
+      $schema['troth_office']['fields']['office_term']['not null'] = FALSE;
+    }
+    // Then target your annoying field and set the 'not null' key to FALSE!
+    if (!empty($schema['troth_office']['fields']['office_open'])) {
+      $schema['troth_office']['fields']['office_open']['not null'] = FALSE;
+    }
+    // Then target your annoying field and set the 'not null' key to FALSE!
+    if (!empty($schema['troth_office']['fields']['office_roles'])) {
+      $schema['troth_office']['fields']['office_roles']['not null'] = FALSE;
+    }
     return $schema;
   }
 
